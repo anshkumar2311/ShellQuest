65c36fde555448116e0552420c59551eb28ea00f
rudraksha007 <rudraman.2805+dev@gmail.com>
2026-07-22 18:13:59 +0530

    remove uneeded files

--- backend/test_db.ts
+++
@@ -1,7 +0,0 @@
-import { PrismaClient } from "./src/generated/prisma/client";
-const prisma = new PrismaClient();
-async function run() {
-  const quizzes = await prisma.quiz.findMany({ include: { questions: true } });
-  console.log(JSON.stringify(quizzes, null, 2));
-}
-run();
--- backend/test_prisma.ts
+++
@@ -1,6 +0,0 @@
-import { prisma } from "./src/db/prisma";
-async function run() {
-  const quiz = await prisma.quiz.findFirst({ include: { questions: { include: { options: true } } } });
-  console.log(JSON.stringify(quiz, null, 2));
-}
-run();
